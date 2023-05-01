0925479807c9d3609718c1286a5ba81b2753879d
HmadyGalo <helenmg1998@gmail.com>
2023-04-30 21:56:16 -0600

    panel y usuario

--- resources/views/uploadfiles.blade.php
+++ resources/views/uploadfiles.blade.php
@@ -10,7 +10,7 @@
 <div class="card shadow mb-4">
   <div class="card-header py-3">
     <div class="d-grid gap-2 d-md-flex justify-content-md-center">
-      <h5 class=" font-weight-bold text-primary">Data de Usuarios</h5>
+      <h5 class=" font-weight-bold text-info">Data de Usuarios</h5>
 
     </div>
 
@@ -22,14 +22,14 @@
           <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
             aria-label="Search" aria-describedby="basic-addon2">
           <div class="input-group-append">
-            <button class="btn btn-primary" type="button">
-              <i class="fas fa-search fa-sm"></i>
+            <button class="btn btn-color" type="button">
+              <i class="fas fa-search fa-sm " style="color:#f4f9fa;"></i>
             </button>
           </div>
         </div>
       </form>
-      <h7 class=" font-weight-bold text-primary">Add</h6>
-        <button class="btn btn-primary btn-md" type="button"><i class="fas fa-plus"></i></button>
+      <h7 class=" font-weight-bold text-info">Add</h6>
+        <button class="btn btn-color btn-md" type="button"><i class="fas fa-plus" style="color:#f4f9fa;"></i></button>
     </div>
   </div>
   <div class="container ">
@@ -52,7 +52,8 @@
             <td></td>
             <td> </td>
             <td> </td>
-            <td><i class="fa fa-edit" aria-hidden="true"> <br></i> <i class="fas fa-trash-alt"></i></td>
+            <td style="color:#02c6dc;"><i class="fa fa-edit" aria-hidden="true"> <br></i> <i
+                class="fas fa-trash-alt"></i></td>
 
           </tr>
 
@@ -62,26 +63,7 @@
 
   </div>
 
-  <div class="card-body">
-    <form action="{{route('files.store')}}" method="POST" enctype="multipart/form-data">
-      @csrf
-      <div class="form-group">
 
-        <input type="file" name="file" id="file">
-        <input type="text" name="Proyecto" value="PROYECTOTEST" hidden>
-
-        @error('file')
-        <small class="text-danger">{{$message}}</small>
-        @enderror
-        <button type="submit" class="btn-primary">Subir Imagen</button>
-      </div>
-
-
-
-
-    </form>
-
-  </div>
 
 
 
