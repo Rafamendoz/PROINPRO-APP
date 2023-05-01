2df61516aee4feb9815f465548462aabafeba76c
Rafael Mendoza <eespino.mendoza@gmail.com>
2023-04-30 21:55:34 -0600

    Modiicaciones en el login

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>SB Admin 2 - Login</title>
+  <title>PROINPRO - Login</title>
   @section('css')
   <!-- Custom fonts for this template-->
   <link href="build/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
