ca560c89e45ef4691a21a84ad37c8ea817367e33
HmadyGalo <helenmg1998@gmail.com>
2023-04-30 21:34:35 -0600

    mejoras

--- resources/views/uploadfiles.blade.php
+++ resources/views/uploadfiles.blade.php
@@ -13,7 +13,21 @@
       <h5 class=" font-weight-bold text-primary">Data de Usuarios</h5>
 
     </div>
+
+
     <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+      <!-- Topbar Search -->
+      <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+        <div class="input-group">
+          <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
+            aria-label="Search" aria-describedby="basic-addon2">
+          <div class="input-group-append">
+            <button class="btn btn-primary" type="button">
+              <i class="fas fa-search fa-sm"></i>
+            </button>
+          </div>
+        </div>
+      </form>
       <h7 class=" font-weight-bold text-primary">Add</h6>
         <button class="btn btn-primary btn-md" type="button"><i class="fas fa-plus"></i></button>
     </div>
@@ -24,15 +38,17 @@
       <table class="table">
         <thead>
           <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Age</th>
-            <th scope="col">Dui</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Usuario</th>
+            <th scope="col">Estado</th>
+            <th scope="col">Intento</th>
             <th scope="col">Action</th>
           </tr>
         </thead>
         <tbody>
 
           <tr>
+            <td></td>
             <td></td>
             <td> </td>
             <td> </td>
