c1588f207465651bc085bbdb25d1bc824eb59d2f
HmadyGalo <helenmg1998@gmail.com>
2023-05-10 15:18:05 -0600

    profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -11,27 +11,47 @@
 
 
 
+
+
 <!-- DataTales Example -->
-<div class="card shadow mb-4">
+<div class="card shadow mb-4 ">
   <div class="card-header bg-white py-0">
     <div class="d-grid gap-3 d-md-flex justify-content-md-center">
       <h1 class=" text-info text-center mt-2"> Mi Perfil</h1>
 
     </div>
-    <div class="col">
-      <div class="card-body text-justify">
-        <h5 class="card-title text-dark">Details</h5>
-        <h6 class="card-subtitle text-dark">Name</h6>
-        <p class="card-text"><small class="text-body-secondary">{{auth()->user()->name}}</small></p>
-        <h6 class="card-subtitle text-dark fw-bold">Apellido</h6>
-        <p class="card-text"><small class="text-body-secondary">{{auth()->user()->lastname}}</small></p>
-        <h6 class="card-subtitle text-dark fw-bold">Rol</h6>
-        <p class="card-text"><small class="text-body-secondary"></small></p>
+    <div class="container ">
+      <div class="row mx-2 p-2 justify-content-center ">
+
+
+        <div class="card card-profile mb-5 mx-3">
+          <div class="row">
+            <div class="col-md-6">
+              <img src="{{ asset('img/undraw_profile_2.svg')}}" class="card-img rounded-circle" alt="person"
+                id="images">
+
+            </div>
+            <div class="col-md-4  mt-5">
+              <div class="card-body text-center">
+                <h1 class="card-title text-info mb-4"><strong><u>Informacion</u></strong></h1>
+                <h3 class="card-text text-info text-justify"><strong>Name: </strong>
+                  {{auth()->user()->name}}</h3><br>
+
+                <h3 class="card-text text-info text-justify"><strong>Apellido: </strong>
+                  {{auth()->user()->lastname}}</h3><br>
+
+                <h3 class="card-text text-info text-justify"><strong>Rol: </strong></h3>
+
+
+              </div>
+            </div>
+          </div>
+        </div>
+
 
       </div>
     </div>
 
-</div>
 
 
-@endsection
+    @endsection
