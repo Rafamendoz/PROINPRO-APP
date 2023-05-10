9d7c923eb28302ba883d569988704741855ad3d3
Rafael Mendoza <eespino.mendoza@gmail.com>
2023-05-09 22:36:16 -0600

    UPDATE RUTAS

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,15 +12,15 @@
   <title>PROINPRO - Login</title>
   @section('css')
   <!-- Custom fonts for this template-->
-  <link href="build/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link rel="stylesheet" href="../css/app.css">
+  <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="css/app.css">
   @stop
   <link
     href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
     rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="build/css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 
