a0583caa8f0cacf14f29ea82b4d238bdb38826ef
HmadyGalo <helenmg1998@gmail.com>
2023-05-10 23:50:47 -0600

    final

--- resources/views/listarUsuario.blade.php
+++ resources/views/listarUsuario.blade.php
@@ -21,53 +21,54 @@
 
 
   </div>
- 
+
 
 
   <div class="card-body">
- 
-          <div class="table-responsive">
-                  <table class="table table-bordered " id="dataTable" width="100%" cellspacing="0">
-                      <thead class="text-center">
-                        <tr>
-                          <th>Nombre</th>
-                          <th>Apellido</th>
-                          <th>Email</th>
-                          <th>Intentos</th>
-                          <th>Usuario</th>
-                          <th>Estado</th>
-                          <th>Creado</th>
-                          <th>Actualizado</th>
-                          <th>Acciones</th>
-                        </tr>
-                      </thead>
 
-                      <tbody class="text-center">
-                        @foreach ($usuarios as $user)
-                          <tr>
-                              <td>{{ $user->name }}</td>
-                              <td>{{ $user->lastname }}</td>
-                              <td>{{$user->email }}</td>
-                              <td>{{$user->intentos}}</td>
-                              <td>{{$user->user }}</td>
-                              <td>{{$user->valor_estado }}</td>
-                              <td>{{$user->created_at }}</td>
-                              <td>{{$user->updated_at }}</td>
-                       
+    <div class="table-responsive">
+      <table class="table table-bordered " id="dataTable" width="100%" cellspacing="0">
+        <thead class="text-center">
+          <tr>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Email</th>
+            <th>Intentos</th>
+            <th>Usuario</th>
+            <th>Estado</th>
+            <th>Creado</th>
+            <th>Actualizado</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
 
-                              <td>
-                                <button class="btn btn-danger btn-sm" type="button" onclick="Delete({{$user->id}})"><i class="fas fa-trash"></i></button>
-                                <a class="btn btn-primary btn-sm" href="../usuario/update/{{$user->user}}"><i class="fas fa-edit"></i></a>
-                              </td>
-                          </tr>
-                        @endforeach
-                      </tbody>
-                    </table>
-            </div>
+        <tbody class="text-center">
+          @foreach ($usuarios as $user)
+          <tr>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->lastname }}</td>
+            <td>{{$user->email }}</td>
+            <td>{{$user->intentos}}</td>
+            <td>{{$user->user }}</td>
+            <td>{{$user->valor_estado }}</td>
+            <td>{{$user->created_at }}</td>
+            <td>{{$user->updated_at }}</td>
 
+
+            <td>
+              <button class="btn btn-danger btn-sm" type="button" onclick="Delete({{$user->id}})"><i
+                  class="fas fa-trash"></i></button>
+              <a class="btn btn-primary btn-sm" href="../usuario/update/{{$user->user}}"><i class="fas fa-edit"></i></a>
+            </td>
+          </tr>
+          @endforeach
+        </tbody>
+      </table>
     </div>
 
-  
+  </div>
+
+
 
 
 
@@ -75,82 +76,82 @@
 
 
 <script>
- function Delete(userid){
-    
-    let data1={};
+function Delete(userid) {
 
-    data1 = {"estado":2};
-    console.log(data1);
- 
-   
+  let data1 = {};
 
-
-    $.ajax({url:"../api/usuarioR/delete/"+userid, type:"put",contentType: "application/json",
-    data: JSON.stringify(data1)
-    , success: function(data){
-        let resultado = data['Estado'];
-        if(resultado=="Exitoso"){
-          const Toast = Swal.mixin({
-              toast: true,
-              position: 'top-end',
-              showConfirmButton: false,
-              timer: 3000,
-              timerProgressBar: true,
-              didOpen: (toast) => {
-                toast.addEventListener('mouseenter', Swal.stopTimer)
-                toast.addEventListener('mouseleave', Swal.resumeTimer)
-              },
-              willClose: () => {
-                location.reload();
-              }
-            })
-
-            Toast.fire({
-              icon: 'success',
-              title: data['Estado']+'!'+' '+data['Descripcion']
-            })
-
-          
+  data1 = {
+    "estado": 2
+  };
+  console.log(data1);
 
 
 
-        }else{  
-          const Toast = Swal.mixin({
-              toast: true,
-              position: 'top-end',
-              showConfirmButton: false,
-              timer: 3000,
-              timerProgressBar: true,
-              didOpen: (toast) => {
-                toast.addEventListener('mouseenter', Swal.stopTimer)
-                toast.addEventListener('mouseleave', Swal.resumeTimer)
-              },
-              willClose: () => {
-                location.reload();
-              }
-            })
 
-            Toast.fire({
-              icon: 'error',
-              title: data['Estado']+'!'+' '+data['Descripcion']
-            })
-           
+  $.ajax({
+    url: "../api/usuarioR/delete/" + userid,
+    type: "put",
+    contentType: "application/json",
+    data: JSON.stringify(data1),
+    success: function(data) {
+      let resultado = data['Estado'];
+      if (resultado == "Exitoso") {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+          },
+          willClose: () => {
+            location.reload();
+          }
+        })
 
-            
+        Toast.fire({
+          icon: 'success',
+          title: data['Estado'] + '!' + ' ' + data['Descripcion']
+        })
 
-        }
-     
+
+
+
+
+      } else {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+          },
+          willClose: () => {
+            location.reload();
+          }
+        })
+
+        Toast.fire({
+          icon: 'error',
+          title: data['Estado'] + '!' + ' ' + data['Descripcion']
+        })
+
+
+
+
+      }
+
 
     }
-    
-    
-    });
-  }
-
-  
 
 
-
+  });
+}
 </script>
 
 
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -15,44 +15,51 @@
 
 <!-- DataTales Example -->
 <div class="card shadow mb-4 ">
-  <div class="card-header bg-white py-0">
-    <div class="d-grid gap-3 d-md-flex justify-content-md-center">
-      <h1 class=" text-info text-center mt-2"> Mi Perfil</h1>
+
+  <div class="card-header py-3">
+    <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+      <h5 class=" font-weight-bold text-info">Mi Perfil</h5>
 
     </div>
-    <div class="container ">
-      <div class="row mx-2 p-2 justify-content-center ">
 
 
-        <div class="card card-profile mb-5 mx-3">
-          <div class="row">
-            <div class="col-md-6">
-              <img src="{{ asset('img/undraw_profile_2.svg')}}" class="card-img rounded-circle" alt="person"
-                id="images">
+
+  </div>
+  <div class="container  ">
+    <div class="row mx-2 p-2 justify-content-center ">
+
+
+      <div class="card card-profile mb-5 mx-3">
+        <div class="row">
+          <div class="col-md-5">
+            <img src="{{ asset('img/undraw_profile_2.svg')}}" class="card-img rounded-circle" alt="person" id="images">
+
+          </div>
+          <div class="col-md-5  mt-5 me-3">
+            <div class="card-body text-justify">
+              <h3 class="card-title text-info mb-4"><strong>Informacion</strong></h3>
+              <h5 class="card-text text-info text-justify"><strong>Nombre: </strong>
+                {{auth()->user()->name}}</h5><br>
+
+              <h5 class="card-text text-info text-justify"><strong>Apellido: </strong>
+                {{auth()->user()->lastname}}</h5><br>
+
+              <h5 class="card-text text-info text-justify"><strong>Rol:
+                </strong> {{$rol_actual[0]['name']}}</h5>
+
 
             </div>
-            <div class="col-md-6  mt-5">
-              <div class="card-body text-center">
-                <h2 class="card-title text-info mb-4"><strong>Informacion</strong></h2>
-                <h5 class="card-text text-info text-justify"><strong>Nombre: </strong>
-                  {{auth()->user()->name}}</h5><br>
 
-                <h5 class="card-text text-info text-justify"><strong>Apellido: </strong>
-                  {{auth()->user()->lastname}}</h5><br>
-
-                <h5 class="card-text text-info text-justify"><strong>Rol:
-                  </strong> {{$rol_actual[0]['name']}}</h5>
-
-
-              </div>
-            </div>
           </div>
         </div>
-
-
       </div>
+
+
     </div>
+  </div>
+
+</div>
 
 
 
-    @endsection
+@endsection
