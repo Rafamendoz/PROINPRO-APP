6ac32f47a7cf271a73ff6258c5c6bda4ba6ffb70
Rafael Mendoza <eespino.mendoza@gmail.com>
2023-05-10 23:38:43 -0600

    PROTECCION DE RUTAS GLOBALES

--- routes/web.php
+++ routes/web.php
@@ -17,55 +17,71 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/admin/{id}', function () {
-    return view('uploadfiles');
-})->name('upArchivos');
 
 
-Route::resource('/admin/files','App\Http\Controllers\FileController');
-
-
-
-Route::post('validate', 'App\Http\Controllers\LoginController@login')->name('validate');
-Route::get('logout', 'App\Http\Controllers\LoginController@logout')->name('logout');
-
-
-
+//RUTAS PUBLICAS
 Route::get("/login", function (){
     return view('index');
 })->name('login');
 
-
-Route::get("usuario/insert",'App\Http\Controllers\UsuarioController@loadList'
-)->name("insertarUsuario");
+Route::post('validate', 'App\Http\Controllers\LoginController@login')->name('validate');
 
 
-Route::get("usuario/update/{id}",'App\Http\Controllers\UsuarioController@loadListUpdateUsuario'
-)->name("actualizarUsuario");
+Route::middleware('auth')->group(function(){
+    Route::middleware('Admin')->group(function(){
+    Route::get("usuario/insert",'App\Http\Controllers\UsuarioController@loadList')->name("insertarUsuario");
+    Route::get("usuario/update/{id}",'App\Http\Controllers\UsuarioController@loadListUpdateUsuario')->name("actualizarUsuario");
+
+    Route::get('/usuarioR/', 'App\Http\Controllers\UsuarioController@getUsuariosRest');
+    Route::get('usuarios/activos', 'App\Http\Controllers\UsuarioController@getUsuariosActivos')->name('getUsuariosActivos');
+    Route::get('usuarios/inactivos', 'App\Http\Controllers\UsuarioController@getUsuariosInaActivos')->name('getUsuariosInactivos');
+    
+    
+    
 
 
+    });
+});
 
 
+Route::middleware('auth')->group(function(){
+Route::get('/admin/{id}', function () {
+    return view('uploadfiles');
+})->name('upArchivos');
 
+Route::resource('/admin/files','App\Http\Controllers\FileController');
+
+Route::get('logout', 'App\Http\Controllers\LoginController@logout')->name('logout');
 Route::get("/createProyect","App\Http\Controllers\ProyectoController@loadList")->name('crearProyecto');
-
 Route::get("/profile/{id}", 'App\Http\Controllers\ModelHasRolesController@getRoleByIdUser'
 )->name('Profile');
-
 Route::get("estado", 'App\Http\Controllers\EstadoController@getEstados')->name('estados');
-
-//RUTAS PARA LA ENTIDAD PROYECTO
  
 Route::get('proyecto/{id}', 'App\Http\Controllers\ProyectoController@getProyectoById');
 Route::get('proyectosR/', 'App\Http\Controllers\ProyectoController@getProyectosRest');
-Route::get('proyectos/', 'App\Http\Controllers\ProyectoController@getProyectos')->middleware('auth')->name('getProyectos');
-
-
-
-Route::get('/usuarioR/', 'App\Http\Controllers\UsuarioController@getUsuariosRest');
-Route::get('usuarios/activos', 'App\Http\Controllers\UsuarioController@getUsuariosActivos')->name('getUsuariosActivos');
-Route::get('usuarios/inactivos', 'App\Http\Controllers\UsuarioController@getUsuariosInaActivos')->name('getUsuariosInactivos');
+Route::get('proyectos/', 'App\Http\Controllers\ProyectoController@getProyectos')->name('getProyectos');
 
 Route::get('/download/{id}/{filename}', 'App\Http\Controllers\FileController@download');
 Route::get('/delete/{id}/{filename}', 'App\Http\Controllers\FileController@destroy');
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//RUTAS PARA LA ENTIDAD PROYECTO
 
